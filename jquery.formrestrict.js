5f84151f7f819cc1ca822227b7b7a59242cedf18
Trey Hunner <treyhunner@gmail.com>
2010-10-18 17:15:58 -0700

    Removed alphanumeric code from jquery.formrestrict.js

--- jquery.formrestrict.js
+++ jquery.formrestrict.js
@@ -1,37 +1,3 @@
-jQuery.fn.alphanumeric = function(r) {
-    alphanumericHelper(this, r, true, true);
-};
-jQuery.fn.numeric = function(r) {
-    alphanumericHelper(this, r, false, true);
-};
-jQuery.fn.alpha = function(r) {
-    alphanumericHelper(this, r, true, false);
-};
-var alphanumericHelper = function(obj, restraints, alpha, numeric) {
-    var regex = "";
-    if (numeric)
-        regex += "0-9";
-    if (alpha) {
-        if (restraints == undefined || !restraints.allcaps)
-            regex += "a-z";
-        if (restraints == undefined || !restraints.nocaps)
-            regex += "A-Z";
-    }
-    if (restraints != undefined && restraints.allow != undefined)
-        regex += RegExp.escape(restraints.allow);
-
-    $(obj).regexRestrict(RegExp("[^"+regex+"]", "g"))
-};
-
-/*
- * Function created by Colin Snover in response to an article by Simon Willison
- * on Regular Expression escaping in JavaScript:
- * http://simonwillison.net/2006/Jan/20/escape/
- */
-RegExp.escape = function(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-};
-
 /*
  * Every time the form field is changed, sanitize its contents with the given
  * function to only allow input of a certain form.
